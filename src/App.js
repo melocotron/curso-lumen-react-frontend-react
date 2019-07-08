0abd9f505946f00972bc0f0129ecc9218d98f86c
Angel Arcia <uriarcia@gmail.com>
2019-07-08 12:39:00 -0500

    obtiene datos del back mediante axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-// import axios from 'axios';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Users</h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Users</h1>
+//     </div>
+//   );
+// }
 
-
+class App extends Component {
+  state = {users: []};
+  render() {
+    const {users} = this.state;
+    return ( 
+      <div className = "App">
+        <h1>Users</h1>
+        {
+          users.map( user => (
+            <li key={user.id.toString()}>{user.name}</li>
+          ))
+        }
+      </div>
+    );
+  }
+  componentDidMount() {
+    const url = 'http://localhost:8085/api/v1/users';
+    axios.get(url, {
+      headers: {
+        'Content-Type': 'applications/json'
+      },
+      data : {}, // issue ! send an empty object as "data"
+    }).then(response => {
+      //console.log(response)
+      this.setState({users: response.data})
+    });
+  }
 }
+
 
 export default App;
